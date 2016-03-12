4f81dbb07f59efaca894b5b87dfc4c73e999eabc
Jake Haas <haasjac@gmail.com>
2016-03-12 00:51:43 -0500

    Health Bars

---
+++ Assets/jakePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jakePlayer : MonoBehaviour {
+
+    public float health;
+
+	// Use this for initialization
+	void Start () {
+        health = 1;
+	}
+	
+	void OnMouseDown() {
+        health -= 0.1f;
+    }
+}
