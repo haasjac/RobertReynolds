0e1a1a44dd9720632226849f439720b467e639c2
M-DOL <mjdolan@umich.edu>
2016-03-13 16:20:48 -0400

    More sprites and script work

---
+++ Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EntityState {NORMAL, ATTACKING};
+
+public class PlayerControl : MonoBehaviour {
+
+	public Sprite[] runLeft;
+	public Sprite[] runRight;
+	public Sprite[] jump;
+	public Sprite[] idle;
+    public Sprite[] crouch;
+
+    SpriteRenderer sr;
+    Rigidbody2D rigid;
+    StateMachine animation_state_machine;
+	StateMachine control_state_machine;
+    public EntityState current_state = EntityState.NORMAL;
+    private float iH, iV;
+    public float moveSpeed = 5f;
+    // Use this for initialization
+    void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animation_state_machine = new StateMachine();
+        animation_state_machine.ChangeState(new StateIdleWithSprite(this, sr, idle[0]));
+        control_state_machine = new StateMachine();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        iV = Input.GetAxis("Vertical");
+        iH = Input.GetAxis("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector3(iH * moveSpeed, 0f, 0f);
+    }
+}
--- Assets/_Scripts/StateMachine.cs
+++ Assets/_Scripts/StateMachine.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-using System.Collections.Generic;
 
 // State Machines are responsible for processing states, notifying them when they're about to begin or conclude, etc.
 public class StateMachine
@@ -61,3 +58,116 @@ public class State
 	// States may call ConcludeState on themselves to end their processing.
 	public void ConcludeState() { state_machine.Reset(); }
 }
+
+// A State that takes a renderer and a sprite, and implements idling behavior.
+// The state is capable of transitioning to a walking state upon key press.
+public class StateIdleWithSprite : State
+{
+	PlayerControl pc;
+	SpriteRenderer renderer;
+	Sprite sprite;
+	
+	public StateIdleWithSprite(PlayerControl pc, SpriteRenderer renderer, Sprite sprite)
+	{
+		this.pc = pc;
+		this.renderer = renderer;
+		this.sprite = sprite;
+	}
+	
+	public override void OnStart()
+	{
+		renderer.sprite = sprite;
+	}
+	
+	public override void OnUpdate(float time_delta_fraction)
+	{
+		if(pc.current_state == EntityState.ATTACKING)
+			return;
+
+		// Transition to walking animations on key press.
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.crouch, 6, KeyCode.DownArrow));
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.jump, 6, KeyCode.UpArrow));
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.runRight, 6, KeyCode.RightArrow));
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.runLeft, 6, KeyCode.LeftArrow));
+	}
+}
+
+// A State for playing an animation until a particular key is released.
+// Good for animations such as walking.
+public class StatePlayAnimationForHeldKey : State
+{
+	PlayerControl pc;
+	SpriteRenderer renderer;
+	KeyCode key;
+	Sprite[] animation;
+	int animation_length;
+	float animation_progression;
+	float animation_start_time;
+	int fps;
+	
+	public StatePlayAnimationForHeldKey(PlayerControl pc, SpriteRenderer renderer, Sprite[] animation, int fps, KeyCode key)
+	{
+		this.pc = pc;
+		this.renderer = renderer;
+		this.key = key;
+		this.animation = animation;
+		this.animation_length = animation.Length;
+		this.fps = fps;
+		
+		if(this.animation_length <= 0)
+			Debug.LogError("Empty animation submitted to state machine!");
+	}
+	
+	public override void OnStart()
+	{
+		animation_start_time = Time.time;
+        if(key == KeyCode.LeftArrow)
+        {
+            pc.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+	}
+	
+	public override void OnUpdate(float time_delta_fraction)
+	{
+		if(pc.current_state == EntityState.ATTACKING)
+			return;
+
+		if(this.animation_length <= 0)
+		{
+			Debug.LogError("Empty animation submitted to state machine!");
+			return;
+		}
+		
+		// Modulus is necessary so we don't overshoot the length of the animation.
+		int current_frame_index = ((int)((Time.time - animation_start_time) / (1.0 / fps)) % animation_length);
+		renderer.sprite = animation[current_frame_index];
+		
+		// If another key is pressed, we need to transition to a different walking animation.
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.crouch, 6, KeyCode.DownArrow));
+		else if(Input.GetKeyDown(KeyCode.UpArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.jump, 6, KeyCode.UpArrow));
+		else if(Input.GetKeyDown(KeyCode.RightArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.runRight, 6, KeyCode.RightArrow));
+		else if(Input.GetKeyDown(KeyCode.LeftArrow))
+			state_machine.ChangeState(new StatePlayAnimationForHeldKey(pc, renderer, pc.runLeft, 6, KeyCode.LeftArrow));
+		
+		// If we detect the specified key has been released, return to the idle state.
+		else if(!Input.GetKey(key))
+			state_machine.ChangeState(new StateIdleWithSprite(pc, renderer, animation[1]));
+	}
+}
+
+// Additional recommended states:
+// StateDeath
+// StateDamaged
+// StateWeaponSwing
+// StateVictory
+
+// Additional control states:
+// NormalMovement.
+// StunnedState.
