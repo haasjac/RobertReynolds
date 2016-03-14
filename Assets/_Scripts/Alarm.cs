c1c2c643b98ba0a58db813e907b1489ad2efbe1f
M-DOL <mjdolan@umich.edu>
2016-03-14 17:47:15 -0400

    Bullet sprite

---
+++ Assets/_Scripts/Alarm.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alarm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0f, 2f, 0f);
+	}
+}
