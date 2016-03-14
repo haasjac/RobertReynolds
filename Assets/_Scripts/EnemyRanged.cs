6431217da09d8d0218b8c759d026bab3210c9ecf
DerSchnurrbart <madumingoni@gmail.com>
2016-03-14 17:34:16 -0400

    turret

---
+++ Assets/_Scripts/EnemyRanged.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyRanged : MonoBehaviour {
+
+    public GameObject player_1;
+    public GameObject player_2;
+    public GameObject projectile;
+
+    public float reload = 2f;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("attack");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(player_1.transform.position);
+	}
+
+
+    public IEnumerator attack() {
+        for (;;) {
+            //spawn projectile
+            GameObject obj = Instantiate<GameObject>(projectile);
+            obj.transform.position = this.transform.position;
+
+            //choose target and aim
+            //Vector3 relativePos = player_1.transform.position - this.transform.position;
+            //obj.transform.rotation = Quaternion.LookRotation(relativePos);
+            //Quaternion rotation = Quaternion.LookRotation(player_1.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+            //obj.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+            obj.transform.LookAt(player_1.transform.position);
+
+            //TEMP: left rotation fix
+            //if (player_1.transform.position.x < this.transform.position.x) {
+           //     this.transform.rotation = new Quaternion(0,0,0,0);
+                //rotation.z -= 0.5f;
+                //obj.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+           // }
+
+            //attack cooldown
+            yield return new WaitForSeconds(reload);
+        }
+    }
+}
---
+++ Assets/_Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float movement_speed = 10f;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //move projectile forward
+        Vector3 pos = this.transform.position;
+        pos += transform.forward * (Time.deltaTime * movement_speed);
+        this.transform.position = pos;
+    }
+
+
+    void OnCollisionEnter(Collision coll) {
+        switch (coll.gameObject.tag) {
+            case "Player":
+                //player.health--;
+                Destroy(this.gameObject);
+                break;
+            default:
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+}
