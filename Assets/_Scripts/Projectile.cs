c84097d765a5f026b75b31b12b314dbe7629726a
M-DOL <mjdolan@umich.edu>
2016-03-15 14:12:11 -0400

    Prefabs and sprites

---
+++ Assets/_Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public float existTime = 5f;
+    public float startTime;
+    public float speed = 3f;
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+        GetComponent<Rigidbody2D>().velocity *= speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - startTime > existTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+    }
+}
