8657d472262e41450ff72db53d628fb8d6562e3f
M-DOL <mjdolan@umich.edu>
2016-04-02 12:37:45 -0400

    Title Scene done and Stick Pony for cowboy level

--- Assets/_Scripts/UI/Title.cs
+++ Assets/_Scripts/UI/Title.cs
@@ -3,66 +3,65 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class Title : MonoBehaviour {
-    public Vector3[] selectLocs = new Vector3[3];
+    public Image[] img = new Image[3];
+    public Sprite[] defaultSprite = new Sprite[3];
+    public Sprite[] selected = new Sprite[3];
+    public GameObject[] ps = new GameObject[3];
     public int selectionNum = 0;
-    public RectTransform selector;
-    Image img;
-    public Sprite ice, fire;
+    AudioSource sound;
 	// Use this for initialization
 	void Start ()
     {
-        selector = transform.FindChild("Selector") as RectTransform;
-        img = selector.gameObject.GetComponent<Image>();
-        selectLocs[0] = new Vector3(-86, -38, 0);
-        selectLocs[1] = new Vector3(-86, -98, 0);
-        selectLocs[2] = new Vector3(-86, -158, 0);
-    }
-    void FixedUpdate()
-    {
-        selector.Rotate(Vector3.forward, 5f);
+        sound = Camera.main.GetComponent<AudioSource>();
+        img[0] = transform.FindChild("Start").GetComponent<Image>();
+        img[1] = transform.FindChild("Controls").GetComponent<Image>();
+        img[2] = transform.FindChild("Level Select").GetComponent<Image>();
     }
     // Update is called once per frame
     void Update()
 {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            img.sprite = fire;
+            sound.PlayOneShot(Resources.Load("Sounds/Ratchet") as AudioClip);
+            img[selectionNum].sprite = defaultSprite[selectionNum];
+            ps[selectionNum].SetActive(false);
             --selectionNum;
+            if (selectionNum == -1)
+            {
+                selectionNum = 2;
+            }
+            else
+            {
+                selectionNum %= 3;
+            }
+            img[selectionNum].sprite = selected[selectionNum];
+            ps[selectionNum].SetActive(true);
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            img.sprite = fire;
+            sound.PlayOneShot(Resources.Load("Sounds/Ratchet") as AudioClip);
+            img[selectionNum].sprite = defaultSprite[selectionNum];
+            ps[selectionNum].SetActive(false);
             ++selectionNum;
+            selectionNum %= 3;
+            img[selectionNum].sprite = selected[selectionNum];
+            ps[selectionNum].SetActive(true);
         }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            img.sprite = ice;
-            --selectionNum;
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            img.sprite = ice;
-            ++selectionNum;
-        }
-        if (selectionNum == -1)
-        {
-            selectionNum = 2;
-        }
+        
         if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.Return))
         {
-            switch(selectionNum % 3)
+            switch(selectionNum)
             {
                 case 0:
                     SceneManager.LoadScene("Level1-1");
                     break;
                 case 1:
-                    SceneManager.LoadScene("Level_Select");
+                    SceneManager.LoadScene("Controls");
                     break;
                 case 2:
-                    SceneManager.LoadScene("Controls");
+                    SceneManager.LoadScene("Level_Select");
                     break;
             }
         }
-        selector.localPosition = selectLocs[selectionNum % 3];
     }
 }
