c92bba76ba39b47a44a8e1e280b2b01f9be9fcbd
M-DOL <mjdolan@umich.edu>
2016-04-04 22:06:27 -0400

    Added woman

---
+++ Assets/FemaleStar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FemaleStar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.right / 100f;
+	}
+}
