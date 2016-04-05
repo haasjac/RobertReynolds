88dc47d8c68133793b82c6234d32221b8a9098ee
M-DOL <mjdolan@umich.edu>
2016-04-05 11:32:38 -0400

    Changes

--- Assets/_Scripts/FemaleStar.cs
+++ Assets/_Scripts/FemaleStar.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class FemaleStar : MonoBehaviour {
     bool collided = false;
+    public bool start = false;
+    public GameObject heartPrefab;
     Animator anim;
 	// Use this for initialization
 	void Start ()
@@ -12,14 +15,25 @@ public class FemaleStar : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if(!collided)
+        if(!collided && start)
         {
-            transform.position += Vector3.right / 100f;
+            transform.position -= Vector3.right / 100f;
         }
     }
     void OnTriggerEnter2D(Collider2D coll)
     {
         collided = true;
         anim.SetBool("talking", true);
+        StartCoroutine(Finish());
+    }
+    public void startWalk()
+    {
+        start = true;
+    }
+    IEnumerator Finish()
+    {
+        Instantiate(heartPrefab, (Whole.S.transform.position + transform.position) / 2f, Quaternion.identity);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Title");
     }
 }
