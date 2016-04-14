303e0f04cf97ea9d58b8124510996e6e8331354f
M-DOL <mjdolan@umich.edu>
2016-04-14 01:48:24 -0400

    Animations

---
+++ Assets/_Scripts/Script Animation/LegacyAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegacyAnimation : MonoBehaviour {
+    Animation anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(!anim.isPlaying)
+        {
+            anim.Play();
+        }
+    }
+}
