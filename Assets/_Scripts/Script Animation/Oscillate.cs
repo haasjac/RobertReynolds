4d9f36093fc68bc32a1cfeeccf278c346213b7d1
M-DOL <mjdolan@umich.edu>
2016-04-20 00:10:57 -0400

    Fixes

--- Assets/_Scripts/Script Animation/Oscillate.cs
+++ Assets/_Scripts/Script Animation/Oscillate.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class Oscillate : MonoBehaviour {
     public float speed = .001f;
+    Vector3 startPos;
 	// Update is called once per frame
+    void Start()
+    {
+        startPos = transform.position;
+    }
 	void Update ()
     {
-        transform.position -= (new Vector3(0, Mathf.Cos(2 * Mathf.PI * Time.timeSinceLevelLoad) * speed, 0f));
+        transform.position = startPos - (new Vector3(0, Mathf.Cos(2 * Mathf.PI * Time.timeSinceLevelLoad) * speed, 0f));
     }
 }
