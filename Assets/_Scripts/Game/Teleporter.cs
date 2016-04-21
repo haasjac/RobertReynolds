7a6f7eb196d283162a4aba2ae2c48434005c75c9
M-DOL <mjdolan@umich.edu>
2016-04-21 13:54:48 -0400

    Sound

---
+++ Assets/_Scripts/Game/Teleporter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject teleporter;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.collider.tag == "Bottom")
+        {
+            UI.S.PlaySound("Teleporter");
+            Bottom.S.container.transform.position = teleporter.transform.position;
+            print("Here");
+        }
+    }
+}
